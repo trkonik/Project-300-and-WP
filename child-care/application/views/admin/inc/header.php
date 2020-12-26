3ba0e2f1ccafc8f95d50ab1baa2aef6416707574
Touhidur Rahman Konik <73713736+trkonik@users.noreply.github.com>
2020-12-27 01:53:15 +0600

    Add files via upload

---
+++ child-care/application/views/admin/inc/header.php
@@ -0,0 +1,122 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Admin Panel</title>
+        <link href="<?=base_url()?>assets/admin/css/styles.css" rel="stylesheet" />
+        <link href="https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap4.min.css" rel="stylesheet" crossorigin="anonymous" />
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/js/all.min.js" crossorigin="anonymous"></script>
+    </head>
+    <body class="sb-nav-fixed">
+        <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
+        <div class="logo p-1 mt-3" >
+						<h1><a class="navbar-brand" href="<?=base_url()?>"><span>C</span>hild-Care <i class="fa fa-plus" aria-hidden="true"></i> <p>Quality Care 4U</p></a></h1>
+					</div>
+            <button class="btn btn-link btn-sm order-1 order-lg-0" id="sidebarToggle" href="#"><i class="fas fa-bars"></i></button>
+            <!-- Navbar Search-->
+            <form class="d-none d-md-inline-block form-inline ml-auto mr-0 mr-md-3 my-2 my-md-0">
+               
+            </form>
+            <!-- Navbar-->
+            <ul class="navbar-nav ml-auto ml-md-0">
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" id="userDropdown" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fas fa-user fa-fw"></i></a>
+                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="userDropdown">
+                        <a class="dropdown-item" href="#">Settings</a>
+                        <a class="dropdown-item" href="#">Activity Log</a>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="<?=base_url()?>admin/logout">Logout</a>
+                    </div>
+                </li>
+            </ul>
+        </nav>
+        <div id="layoutSidenav">
+            <div id="layoutSidenav_nav">
+                <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
+                    <div class="sb-sidenav-menu">
+                        <div class="nav">
+ 
+                            <a class="nav-link" href="<?=base_url()?>Admin">
+                                <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
+                                Dashboard
+                            </a>
+
+                             <a class="nav-link" href="<?=base_url()?>admin/add-user">
+                                <div class="sb-nav-link-icon"><i class="fas fa-plus"></i></div>
+                               Add User
+                            </a>
+
+
+                             <a class="nav-link" href="<?=base_url()?>admin/view-user">
+                                <div class="sb-nav-link-icon"><i class="fas fa-eye"></i></div>
+                               View User
+                            </a>
+                            
+                            <a class="nav-link" href="<?=base_url()?>admin/add-vitamin">
+                                <div class="sb-nav-link-icon"><i class="fas fa-plus"></i></div>
+                               Add Vitamin
+                            </a>
+
+
+                             <a class="nav-link" href="<?=base_url()?>admin/view-Vitamin">
+                                <div class="sb-nav-link-icon"><i class="fas fa-eye"></i></div>
+                               View Vitamin
+                            </a>
+                            
+                            
+                            <a class="nav-link" href="<?=base_url()?>admin/add-symptoms">
+                                <div class="sb-nav-link-icon"><i class="fas fa-plus"></i></div>
+                               Add Symptoms
+                            </a>
+
+
+                             <a class="nav-link" href="<?=base_url()?>admin/view-symptoms">
+                                <div class="sb-nav-link-icon"><i class="fas fa-eye"></i></div>
+                               View Symptoms
+                            </a>
+                            </a>
+                            <a class="nav-link" href="<?=base_url()?>admin/add-organ">
+                                <div class="sb-nav-link-icon"><i class="fas fa-plus"></i></div>
+                               Add Organ
+                            </a>
+
+
+                             <a class="nav-link" href="<?=base_url()?>admin/view-organ">
+                                <div class="sb-nav-link-icon"><i class="fas fa-eye"></i></div>
+                               View Organ
+                            </a>
+                            <a class="nav-link" href="<?=base_url()?>admin/add-blog">
+                                <div class="sb-nav-link-icon"><i class="fas fa-plus"></i></div>
+                               Add Blog
+                            </a>
+                             <a class="nav-link" href="<?=base_url()?>admin/view-blog">
+                                <div class="sb-nav-link-icon"><i class="fas fa-eye"></i></div>
+                               View Blog
+                            </a>
+                            <a class="nav-link" href="<?=base_url()?>admin/add-detail">
+                                <div class="sb-nav-link-icon"><i class="fas fa-plus"></i></div>
+                               Add Detail
+                            </a>
+                             <a class="nav-link" href="<?=base_url()?>admin/view-detail">
+                                <div class="sb-nav-link-icon"><i class="fas fa-eye"></i></div>
+                               View Detail
+                            </a>
+                            <a class="nav-link" href="<?=base_url()?>admin/add-alert">
+                                <div class="sb-nav-link-icon"><i class="fas fa-plus"></i></div>
+                               Add Alert
+                            </a>
+                             <a class="nav-link" href="<?=base_url()?>admin/view-alert">
+                                <div class="sb-nav-link-icon"><i class="fas fa-eye"></i></div>
+                               View Alert
+                            </a>
+                            
+                             
+                        </div>
+                    </div>
+                   
+                </nav>
+            </div>
